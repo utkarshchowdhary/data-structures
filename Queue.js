e7c4400d6fc0dc8dc26941f21d9a7bf62374fe46
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-03-09 15:17:28 +0530

    queue implementation

---
+++ Queue.js
@@ -0,0 +1,61 @@
+/*
+  -A Queue is a linear data structure in which insertion and deletion happens on different ends 
+  of the list.
+  -Queues are based on the FIFO principle, i.e., the least recently added element, is the first element to 
+  come out of the list.
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  peek() {
+    return this.first.value;
+  }
+
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (this.first) {
+      this.last.next = node;
+      this.last = node;
+    } else {
+      this.first = node;
+      this.last = node;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (this.first) {
+      const current = this.first;
+      if (this.first === this.last) {
+        this.last = null;
+      }
+      this.first = this.first.next;
+      this.size--;
+      return current.value;
+    }
+    return null;
+  }
+}
+
+const queue = new Queue();
+
+queue.enqueue("QuickSilver");
+queue.enqueue("Wanda");
+queue.enqueue("Vision");
+
+queue.dequeue();
+
+console.dir(queue, { depth: null });
