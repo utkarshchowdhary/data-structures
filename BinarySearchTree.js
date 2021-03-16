ab8b54c2c6ca960d3d3f9ac9d7d8362140494290
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-03-16 18:43:22 +0530

    delete node in bst iteratively

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -96,6 +96,90 @@ class BinarySearchTree {
     }
   }
 
+  delete_inorder_successor(node) {
+    let parent = node;
+    node = node.right;
+
+    while (node.left) {
+      parent = node;
+      node = node.left;
+    }
+
+    if (parent.right === node) {
+      // node is to the right of the parent (node was already at minimum).
+      parent.right = node.right;
+    } else if (parent.left === node) {
+      // node is to the left of the parent.
+      parent.left = node.right;
+    }
+
+    return node;
+  }
+
+  delete_iteratively(value) {
+    let parent = null;
+    let current = this.root;
+
+    while (current && current.value !== value) {
+      parent = current;
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      }
+    }
+
+    if (!current) return;
+
+    if (current.left && current.right) {
+      // current node has both children.
+      // delete the node with the minimum value in right subtree and place it's value to the current node.
+      current.value = this.delete_inorder_successor(current).value;
+    } else if (current.left) {
+      // current node has only left child.
+      if (!parent) {
+        // current node is root with only left child.
+        this.root = current.left;
+      } else {
+        if (parent.left === current) {
+          // current node is to the left of the parent.
+          parent.left = current.left;
+        } else if (parent.right === current) {
+          // current node is to the right of the parent.
+          parent.right = current.left;
+        }
+      }
+    } else if (current.right) {
+      // current node has only right child.
+      if (!parent) {
+        // current node is root with only right child.
+        this.root = current.right;
+      } else {
+        if (parent.left === current) {
+          // current node is to the left of the parent.
+          parent.left = current.right;
+        } else if (parent.right === current) {
+          // current node is to the right of the parent.
+          parent.right = current.right;
+        }
+      }
+    } else {
+      // current node has no children.
+      if (!parent) {
+        // current node is root with no left or right.
+        this.root = null;
+      } else {
+        if (parent.left === current) {
+          // current node is to the left of the parent.
+          parent.left = null;
+        } else if (parent.right === current) {
+          // current node is to the right of the parent.
+          parent.right = null;
+        }
+      }
+    }
+  }
+
   find_min(node) {
     let current_node = node;
     while (current_node.left) {
