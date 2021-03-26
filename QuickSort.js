a45351e952a5ab4929e0c698de2edb023b8d8d27
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-03-26 15:23:41 +0530

    implement quick sort

---
+++ QuickSort.js
@@ -0,0 +1,20 @@
+function quickSort(a, low, high) {
+  if (low < high) {
+    let pivot = a[high];
+    let partitionIndex = low;
+    for (let i = low; i < high; i++) {
+      if (a[i] <= pivot) {
+        [a[partitionIndex], a[i]] = [a[i], a[partitionIndex]];
+        partitionIndex++;
+      }
+    }
+    [a[partitionIndex], a[high]] = [a[high], a[partitionIndex]];
+
+    quickSort(a, low, partitionIndex - 1);
+    quickSort(a, partitionIndex + 1, high);
+    return a;
+  }
+}
+
+const a = [8, 7, 6, 1, 0, 9, 2];
+console.log(quickSort(a, 0, a.length - 1));
