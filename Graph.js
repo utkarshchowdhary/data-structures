9914e5abd4e26ca944dbb2e857db3efc6ee8b6a1
utkarshchowdhary <utkarshchowdhary4140@gmail.com>
2021-03-28 00:25:36 +0530

    switched to integer based nodes

--- Graph.js
+++ Graph.js
@@ -113,27 +113,27 @@ const graph = new Graph();
    \  /
     0
 */
-graph.addVertex("0");
-graph.addVertex("1");
-graph.addVertex("2");
-graph.addVertex("3");
-graph.addVertex("4");
-graph.addVertex("5");
-graph.addVertex("6");
-graph.addEdge("0", "1");
-graph.addEdge("0", "2");
-graph.addEdge("1", "2");
-graph.addEdge("1", "3");
-graph.addEdge("2", "4");
-graph.addEdge("3", "4");
-graph.addEdge("4", "5");
-graph.addEdge("5", "6");
+graph.addVertex(0);
+graph.addVertex(1);
+graph.addVertex(2);
+graph.addVertex(3);
+graph.addVertex(4);
+graph.addVertex(5);
+graph.addVertex(6);
+graph.addEdge(0, 1);
+graph.addEdge(0, 2);
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(2, 4);
+graph.addEdge(3, 4);
+graph.addEdge(4, 5);
+graph.addEdge(5, 6);
 
 console.log(graph);
 
 console.log("BFS");
-graph.BFS("2");
+graph.BFS(2);
 console.log("\nDFS");
-graph.DFS("2");
+graph.DFS(2);
 console.log("\nDFS RECURSIVE");
-graph.DFSRecursive("2");
+graph.DFSRecursive(2);
