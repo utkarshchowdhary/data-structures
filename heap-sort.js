98dfed7926128d6d390da8f06344f4f9036982a2
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2021-07-03 08:42:49 +0530

    refactor

--- heap-sort.js
+++ heap-sort.js
@@ -6,7 +6,7 @@ function heapSort(arr) {
     heapify(arr, n, i);
   }
 
-  for (let i = n - 1; i >= 0; i--) {
+  for (let i = n - 1; i > 0; i--) {
     [arr[i], arr[0]] = [arr[0], arr[i]];
 
     // Heapify root node
