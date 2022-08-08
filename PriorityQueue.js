dd63fbbdccdab27ff7f9d3f7b2c0ed7ee598a805
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2022-08-08 22:17:17 +0530

    implement PriorityQueue by disregarding entry count so when the priorities on nodes are same, they are served arbitrarily

--- PriorityQueue.js
+++ PriorityQueue.js
@@ -1,25 +1,21 @@
 /**
  * In Priority Queue the data organized by importance rather than when it was added.
- * A node with higher priority is served before a node with lower priority.
- * When the priorities on nodes are same, they are served according to the order in which they were added.
+ * A node with higher priority is served before a node with lower priority,
+ * when the priorities on nodes are same, they are served arbitrarily.
  */
 
 class Node {
-  constructor(value, priority, serialNumber) {
+  constructor(value, priority) {
     this.value = value
     this.priority = priority
-    this.serialNumber = serialNumber
   }
 }
 
 class PriorityQueue {
-  constructor(comparator = (a, b) => a - b) {
+  constructor(comparator = (a, b) => a < b) {
     this.heap = []
-    this.identity = 0
-    this.comparator = (a, b) => {
-      const value = comparator(a.priority, b.priority)
-      return (value === 0 ? a.serialNumber - b.serialNumber : value) < 0
-    }
+    this.comparator = (i, j) =>
+      comparator(this.heap[i].priority, this.heap[j].priority)
   }
 
   swap(i, j) {
@@ -43,7 +39,7 @@ class PriorityQueue {
   }
 
   enqueue(value, priority) {
-    const node = new Node(value, priority, ++this.identity)
+    const node = new Node(value, priority)
 
     // adding a new node can be done by simply pushing it onto an array
     // then "bubbling up" the new node if its priority is greater than its parent's priority.
@@ -54,7 +50,7 @@ class PriorityQueue {
     // When index is at 0, can not go up any further,
     // if the node's priority is greater than its parent's priority,
     // swap them and save its parent's index.
-    while (i > 0 && this.comparator(this.heap[i], this.heap[this.parent(i)])) {
+    while (i > 0 && this.comparator(i, this.parent(i))) {
       this.swap(this.parent(i), i)
       i = this.parent(i)
     }
@@ -86,29 +82,18 @@ class PriorityQueue {
       // if the left child exists,
       // set the index of the left child to the next
       // if its priority is greater than the priority of the i-th node
-      // or if its priority is equal to the i-th node's
-      // but it was inserted before the i-th node.
-      if (
-        leftIndex < n &&
-        this.comparator(this.heap[leftIndex], this.heap[i])
-      ) {
+      if (leftIndex < n && this.comparator(leftIndex, i)) {
         next = leftIndex
       }
 
       // if the right child exists,
       // set the index of the right child to the next
       // if its priority is greater than the greatest priority between the i-th node and its left child
-      // or if its priority is equal to the next node's
-      // but it was inserted before the next node.
-      if (
-        rightIndex < n &&
-        this.comparator(this.heap[rightIndex], this.heap[next])
-      ) {
+      if (rightIndex < n && this.comparator(rightIndex, next)) {
         next = rightIndex
       }
 
       // if the i-th node's priority is greater than the priorities of its children,
-      // or if the priorities are the same, it was inserted before its children
       // break out of the loop.
       if (next === i) break
 
@@ -133,14 +118,12 @@ class PriorityQueue {
 // pq.enqueue(6, 3)
 // pq.enqueue(5, 2)
 
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
+// while (!pq.isEmpty()) {
+//   console.log(pq.dequeue())
+// }
 
 // max-priority queue
-// const pq = new PriorityQueue((a, b) => b - a)
+// const pq = new PriorityQueue((a, b) => a > b)
 /*
       4
     6  31
@@ -152,10 +135,8 @@ class PriorityQueue {
 // pq.enqueue(6, 3)
 // pq.enqueue(5, 2)
 
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
-// console.log(pq.dequeue())
+// while (!pq.isEmpty()) {
+//   console.log(pq.dequeue())
+// }
 
 exports.PriorityQueue = PriorityQueue
